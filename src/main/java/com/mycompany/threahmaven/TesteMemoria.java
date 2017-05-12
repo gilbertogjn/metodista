382babe8241d82e0658b67942b39cd65584e2620
Gilberto <gilbertogjn@gmail.com>
2017-05-12 01:49:34 -0400

    Novo Commit

---
+++ src/main/java/com/mycompany/threahmaven/TesteMemoria.java
@@ -0,0 +1,23 @@
+package com.mycompany.threahmaven;
+
+public class TesteMemoria {
+    
+    public static void main(String[] args) {
+        
+        Runtime rt = Runtime.getRuntime();
+        
+        System.out.println("Memoria livre..: " + rt.freeMemory());
+        
+        String as = new String ("Teste de exemplo");
+        String es = new String("Teste de exemplo 2");
+        
+        System.out.println("Memoria livre..: " + rt.freeMemory());
+        
+        es = as;
+        
+        System.gc();
+        
+        System.out.println("Memoria livre..: " + rt.freeMemory());
+    }
+    
+}
